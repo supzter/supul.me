7852a9fad9efe250d4009e9d5c2705427ad030c2
Supul Amarakoon <supul@oqpi.co>
2019-10-16 19:38:42 -0400

    New portfolio v 1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Setup
+
+- Make sure you have Jekyll and Ruby. If you don't, follow the instructions in the [here](https://jekyllrb.com/) to set it up.
+
+- Clone this repository to your machine.
+
+- Run `bundle install` to install the dependencies.
+
+# Development
+
+- Run `bundle exec jekyll serve` to start the development server.
+
+# Deploy
+
+- Run `bundle exec jekyll build` to generate the static site.
+
+- Upload the contents in `_site` to your web server.
+
+
+
---
+++ _assets/js/app.js
@@ -0,0 +1,37 @@
+function stickyHeader() {
+    $(window).on('scroll', function(event) {
+        var scrollValue = $(window).scrollTop();
+        var windowHeight = $( window ).height();
+        var heightMark = $('.height-mark').position().top;
+        if ( scrollValue > heightMark) {
+             $('.navbar').removeClass('navbar-dark');
+             $('.navbar').addClass('navbar-light');
+             $('.navbar').addClass('bg-light');
+     
+        } 
+        else{
+            $('.navbar').removeClass('navbar-light');
+            $('.navbar').removeClass('bg-light');
+             $('.navbar').addClass('navbar-dark');
+    
+             
+        }
+    });
+}
+
+function setEmailAndPhone() {
+    var email = atob("c3VwdWwuYW1hcmFrb29uQGdtYWlsLmNvbQ==")
+    var phone = atob("KDY0NykgNTY4IDIyNTE=")
+    var $emailElement = $('span#email-text')
+    var $phoneElement = $('span#phone-text')
+    setTimeout(function() {
+        $emailElement.text(email);
+        $phoneElement.text(phone);
+    }, 1000)
+}
+
+$(function() {
+    stickyHeader();
+    setEmailAndPhone();
+    AOS.init();
+});
